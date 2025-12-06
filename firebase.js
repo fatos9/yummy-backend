f2ef82d47dd969b24e31254a463303b53d37bffb
Fatos <isfatos1@gmail.com>
2025-12-06 21:09:06 +0300

    Fix firebase base64 decode + update backend

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 import admin from "firebase-admin";
 
-// Base64 → JSON decode
-const decodedServiceAccount = Buffer.from(
+// Base64 → UTF-8 → JSON
+const decoded = Buffer.from(
   process.env.FIREBASE_SERVICE_ACCOUNT,
   "base64"
 ).toString("utf-8");
 
-const serviceAccount = JSON.parse(decodedServiceAccount);
+const serviceAccount = JSON.parse(decoded);
 
 if (!admin.apps.length) {
   admin.initializeApp({
