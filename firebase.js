f31d05903e57d88f79adb948c6152910ea328657
Fatos <isfatos1@gmail.com>
2025-12-06 21:58:47 +0300

    Fix firebase base64 and ignore json keys

--- firebase.js
+++ firebase.js
@@ -1,9 +1,6 @@
 import admin from "firebase-admin";
 
-// Base64 decode → JSON string elde et
 const decoded = Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, "base64").toString("utf8");
-
-// JSON parse → gerçek service account nesnesi
 const serviceAccount = JSON.parse(decoded);
 
 if (!admin.apps.length) {
