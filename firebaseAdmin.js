ba78d2a3298461a86678239d7db917ad799e2d7d
Fatos <isfatos1@gmail.com>
2025-12-07 00:54:45 +0300

    firebase admin eklendi ve auth düzeltildi

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,9 +1,11 @@
 import admin from "firebase-admin";
 
-// 🔥 Service Account otomatik alınacak şekilde ayar
+const decoded = Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, "base64").toString("utf8");
+const serviceAccount = JSON.parse(decoded);
+
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert(serviceAccount)
   });
 }
 
